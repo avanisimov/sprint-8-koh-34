7893a9a5d593e8359e86f062615fdcce9c458fec
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2025-10-06 20:14:30 +0300

    Sprint 11: new task  243235

--- app/src/main/java/ru/yandex/practicum/sprint8koh34/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh34/MainActivity.kt
@@ -17,6 +17,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Log.d("TAG", "onCreate: 5")
+        Log.d("TAG", "onCreate: 51231234234")
     }
 }
